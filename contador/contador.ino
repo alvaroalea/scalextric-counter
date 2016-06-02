407f1e6c5991983fb6b915a88d6c557f36f13d45
Alvaro Alea <alvaroalea@gmail.com>
2016-06-02 12:58:03 +0200

    rev 1

--- contador/contador.ino
+++ contador/contador.ino
@@ -1,8 +1,19 @@
-#include <TM1638.h>
-TM1638 module(7, 4, 2);
-int vueltas1 = 10;
-int vueltas2 = 10;
+/* (C) 2014 Alvaro Alea Fernandez <alvaroalea@gmail.com>  *
+ * Distribuido con licencia GPL Version 3                 */
 
+#include <TM1638.h> // ckeck https://code.google.com/p/tm1638-library/
+TM1638 module(7, 4, 2);
+
+#define ST_STOP 0
+#define ST_RUN 50
+int state = 0 ;
+
+int tick = 0;
+int vueltas1 ; 
+int vueltas2 ;
+int nvueltas = 11; //10 (+1) vueltas por defecto, hay que tener en cuenta que segun sales descuenta una.
+
+// esto imprime el nº de vueltas en el display.
 void vueltas(){
   int d1,d2,d3,d4;
   d2 = vueltas1 % 10;
@@ -12,10 +23,15 @@ void vueltas(){
   
   module.setDisplayDigit(d1, 0, false);
   module.setDisplayDigit(d2, 1, false);
+  module.clearDisplayDigit(2, false);
+  module.clearDisplayDigit(3, false);
+  module.clearDisplayDigit(4, false);
+  module.clearDisplayDigit(5, false); 
   module.setDisplayDigit(d3, 6, false);
   module.setDisplayDigit(d4, 7, false);
 }
 
+// La inicializacion y banner de presentacion.
 void setup() {
   // put your setup code here, to run once:
   int i;
@@ -31,79 +47,141 @@ void setup() {
   }
   module.clearDisplay();              //clears the display from garbage if any
   module.setupDisplay(true,7);
-  vueltas();
-  
-
-  int tecla;
-
-  do {
-    tecla = module.getButtons();
-    } while (tecla != 0b00000001);
-  do {
-    tecla = module.getButtons();
-    } while (tecla != 0b00000000);
-
-  module.setLEDs(0x0003);
-  delay(1000);
-  module.setLEDs(0x000F);
-  delay(1000);
-  module.setLEDs(0x003F);
-  delay(1000);
-  module.setLEDs(0x00FF);
-  delay(1000);
-  module.setLEDs(0xFF00);
-
 }
 
-int pos1 = 0;
-int pos2 = 0;
-int in1 = 0;
-int in2 = 0;
-int cambio =0;
-void loop() {
-  // put your main code here, to run repeatedly: 
-in1 = digitalRead(6);
-in2 = digitalRead(5);
-Serial.print(in1);
-Serial.print(in2);
+// Devuelve el bit de la tecla pulsada cuando esta se suelta.
+int leetecla(){
+static int tecla_prev = 0;
+int tecla; 
+int r= 0;
+tecla = module.getButtons();
+if (((tecla_prev & 0x01)!=0) && ((tecla & 0x01)==0)) r += 0x01 ;
+if (((tecla_prev & 0x02)!=0) && ((tecla & 0x02)==0)) r += 0x02 ;
+if (((tecla_prev & 0x04)!=0) && ((tecla & 0x04)==0)) r += 0x04 ;
+if (((tecla_prev & 0x08)!=0) && ((tecla & 0x08)==0)) r += 0x08 ;
+if (((tecla_prev & 0x10)!=0) && ((tecla & 0x10)==0)) r += 0x10 ;
+if (((tecla_prev & 0x20)!=0) && ((tecla & 0x20)==0)) r += 0x20 ;
+if (((tecla_prev & 0x40)!=0) && ((tecla & 0x40)==0)) r += 0x40 ;
+if (((tecla_prev & 0x80)!=0) && ((tecla & 0x80)==0)) r += 0x80 ;
+tecla_prev = tecla;
+return r;  
+}
+  
 
+// simple rutina que lee flancos en los sensores, como se llama aprox cada 10ms, actua ademas de antirebote.
+int leesensores(){
+static int pos1 = 0;
+static int pos2 = 0;
+int r=0;
+int in1;
+int in2;
+
+in1 = digitalRead(6); // recordad, con pull-up, =1 en reposo, =0 activa.
+in2 = digitalRead(5);
 if ((pos1 ==0) && (in1 == 0)) {
   pos1 = 1;
+  }
+if ((pos1 ==1) && (in1 == 1)) {
+  pos1 = 0;
+  r = r + 0b0001;
   }
 if ((pos2 ==0) && (in2 == 0)) {
   pos2 = 1;
   }
-if ((pos1 ==1) && (in1 == 1)) {
-  pos1 = 0;
+if ((pos2 ==1) && (in2 == 1)) {
+  pos2 = 0;
+  r = r + 0b0010;
+  }
+
+return r;
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+int teclas;
+int sensores;
+int cambio =0;
+
+teclas=leetecla();
+if ((teclas & 0x01)!=0) { // empezar la carrera
+  vueltas1 = nvueltas;
+  vueltas2 = nvueltas;
+  tick = 0 ;
+  state = ST_RUN ;
+  cambio = 1;
+}
+
+if (state >= 50) {  //Los modos >=50 el temporizador corre.
+  tick +=1;
+  // control de semaforo
+  if (tick == 1) {
+     module.setLEDs(0x0000);
+  } else if (tick == 100) {
+     module.setLEDs(0x0003);
+  } else if (tick == 200) {
+     module.setLEDs(0x000F);
+  } else if (tick == 300) {
+     module.setLEDs(0x003F);
+  } else if (tick == 400) {
+     module.setLEDs(0x00FF);
+  } else if (tick == 500) { // LED EN VERDE USAR EL VALOR MAS ABAJO
+     module.setLEDs(0xFF00);
+  } else if (tick == 600) {
+     module.setLEDs(0xFF00);
+  } else if (tick == 800) {
+     module.setLEDs(0x0000);
+  }
+
+sensores=leesensores();
+// Evaluamos salida en falso
+if (tick < 500){
+  if ((sensores & 0b0001)!=0) {
+     module.setDisplayToString("FAUL    ");    //prints the banner
+     cambio = 0;
+     state = ST_STOP;
+  }
+  if ((sensores & 0b0010)!=0) {
+     module.setDisplayToString("    FAUL");    //prints the banner
+     cambio = 0;
+     state = ST_STOP;
+  }
+} else {
+// contador de vueltas
+if ((sensores & 0b0001)!=0) {
   vueltas1 = vueltas1 -1;
   cambio =1;
   }
-if ((pos2 ==1) && (in2 == 1)) {
-  pos2 = 0;
+if ((sensores & 0b0010)!=0) {
   vueltas2 = vueltas2 -1;
   cambio =1;
-  }
- 
-if (cambio==1){
-  cambio =0;
-  vueltas();
-  module.setLEDs(0x0000);
   }  
+}
 
+// Evaluamos si alguno gana
 if (vueltas1 ==0) {
     if (vueltas2 == 0) {
        module.setDisplayToString("-Empate-");    //prints the banner
-       delay(5000);
+       cambio = 0;
+       state = ST_STOP;
     } else {
        module.setDisplayToString("GANA    ");    //prints the banner
-       delay(5000);
+       cambio = 0;
+       state = ST_STOP;
     }
 } else {
     if (vueltas2 == 0) {
        module.setDisplayToString("    GANA");    //prints the banner
-       delay(5000);
+       cambio = 0;
+       state = ST_STOP;
     }
 }    
+
+} // if (state >= 50)
   
+if (cambio==1){
+  cambio = 0;
+  vueltas();
+  }  
 delay(10);
 }
